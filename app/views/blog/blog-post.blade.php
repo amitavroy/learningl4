ffacb217b7c49d89b6f51111f3d29c9a2db7cfe7
Amitav Roy <reachme@amitavroy.com>
2014-02-15 09:50:09 +0530

    Fixed the size of the content section

--- app/views/blog/blog-post.blade.php
+++ app/views/blog/blog-post.blade.php
@@ -1,14 +1,16 @@
 @layout('html')
 
 @section('content')
-<?php //echo '<pre>' . print_r($post, true) . '</pre>'; ?>
-<h1 class="post-title">
-  {{ $post['content']->nodeTitle }}
-</h1>
+<div class="col-md-8">
+  <?php //echo '<pre>' . print_r($post, true) . '</pre>'; ?>
+  <h1 class="post-title">
+    {{ $post['content']->nodeTitle }}
+  </h1>
 
-<div class="post content content-wrapper">
-  <div class="body">
-    {{ $post['content']->nodeBody }}
+  <div class="post content content-wrapper">
+    <div class="body">
+      {{ $post['content']->nodeBody }}
+    </div>
   </div>
 </div>
 @stop
