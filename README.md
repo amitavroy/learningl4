cd0987a81c1dc6bd802ce0b355815164bda8dc19
Amitav Roy <reachme@amitavroy.com>
2014-07-29 16:27:15 +0530

    Removed the whole code base, will restart with a fresh install

--- README.md
+++
@@ -1,2 +0,0 @@
-Learning Laravel 4
-
--- public/assets/js/dev/sidebar.js
+++
@@ -1,9 +0,0 @@
-var latestArticles = [
-  {'name' : 'This is to add another one'},
-  {'name' : 'Lorem ipsum dolor sit amet'}
-];
-
-/* the sidebar controller*/
-myApp.controller('SidebarController', function($scope) {
-  $scope.sideBarArticles = latestArticles;
-});
