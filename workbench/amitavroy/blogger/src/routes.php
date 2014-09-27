50ac1c1d0cff38502ec8f416bbb00c07509e0c65
Amitav Roy <reachme@amitavroy.com>
2014-09-27 19:53:21 +0530

    Removing old code base

--- workbench/amitavroy/blogger/src/routes.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-Route::get('blog/list', 'BloggerController@handleBlogListingPage');
-Route::get('blog/view/{id}', 'BloggerController@handleBlogSinglePage');
--- workbench/amitavroy/blogger/src/views/blog-list.blade.php
+++
@@ -1,7 +0,0 @@
-@section('content')
-    <h2>Blog listing</h2>
-    
-    @foreach ($blogposts as $blogpost)
-        @include('blogger::blog-teaser', array('blogpost' => $blogpost))
-    @endforeach
-@stop
